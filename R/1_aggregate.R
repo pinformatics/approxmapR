15f9edc145566b8da832652e2ac7042712d54cb0
Corey-Bryant <coreybryant10@gmail.com>
2021-08-25 11:49:22 -0400

    Fixed aggregation bug, modified find_optimal_k function, added plot_ktable, and plot_silhouette
    Fixed aggregation bug that was introduced in the previous version;
    
    Modified find_optimal_k() to no longer plot cluster validation measure and added a silhouette information data structure to the out put. The silhouette information data structure contains the individual silhouette scores and the cluster the individual belongs too;
    
    Add plot_ktable() to plot and export graphs of supported cluster validation measures which is returned from the find_optimal_k() function;
    
    Add plot_silhouette() to visually assess the individual and cluster silhouette scores - also exports graphs and the silhouette information table if desired

--- R/1_aggregate.R
+++ R/1_aggregate.R
@@ -268,12 +268,12 @@ convert_to_sequence <- function(df_seq) {
     # this method has 2
     # mutate calls to do that all while ensuring the classes are appropriately maintained
     df_seq <-
-        df_seq %>% group_by(id) %>% nest(nested_id = c(period, event)) %>% mutate(
+        df_seq %>% group_by(id) %>% nest(.key = "nested_id") %>% mutate(
             sequence = map(nested_id,
                            function(df_id) {
                                seqs <-
                                    df_id %>% group_by(period) %>%
-                                   nest(list_data = event) %>%
+                                   nest(.key = "list_data") %>%
                                    mutate(seqs = map(list_data,
                                                      function(itemset) {
                                                          events <- itemset$event
